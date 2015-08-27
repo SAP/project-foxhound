fd7225f0e5cac4c84b10da04d77b23bdaea35035
unknown <D062701@QKAN31208570A.emea.global.corp.sap>
2015-08-27 13:16:39 +0200

    branding

--- browser/branding/taint/configure.sh
+++ browser/branding/taint/configure.sh
@@ -3,3 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 MOZ_APP_DISPLAYNAME=Taintfox
+
+# the following vars should create a unique profile for tainted browsers
+MOZ_APP_VENDOR=SAP
+MOZ_APP_NAME=taintfox
