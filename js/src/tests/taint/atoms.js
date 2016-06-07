14a15815e7d44f8c92f0e7b98bdcefa0f1551350
Samuel Groß <samuel.gross@sap.com>
2016-06-07 15:29:49 +0200

    New tests added

--- js/src/tests/taint/atoms.js
+++ js/src/tests/taint/atoms.js
@@ -4,6 +4,27 @@ function atomTaintTest() {
 
     assertNotTainted(atom);
     assertTainted(str);
+
+    // Test functions that could produce (tainted) atoms.
+    var untaintedStr = randomString();
+    var taintedStr = taint(untaintedStr);
+    var index = rand() % untaintedStr.length;
+
+    assertTainted(taintedStr[i]);
+    assertNotTainted(untaintedStr[i]);
+    assertTainted(taintedStr[i.toString()]);
+    assertNotTainted(untaintedStr[i.toString()]);
+
+    assertTainted(taintedStr.charAt(i));
+    assertNotTainted(untaintedStr.charAt(i));
+
+    assertTainted(taintedStr.substr(i, 1));
+    assertNotTainted(untaintedStr.substr(i, 1));
+
+    var untaintedStrings = untaintedStr.split('');
+    var taintedStrings = taintedStr.split('');
+    assertTainted(taintedStrings[i]);
+    assertNotTainted(untaintedStrings[i]);
 }
 
 runTaintTest(atomTaintTest);
--- js/src/tests/taint/charAt.js
+++
@@ -1,9 +0,0 @@
-function charAtTest() {
-    var str = randomTaintedString();
-    assertTainted(str.charAt(rand() % str.length));
-}
-
-runTaintTest(charAtTest);
-
-if (typeof reportCompare === "function")
-  reportCompare(true, true);
