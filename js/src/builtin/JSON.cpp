f833e4f9853c6353a490c0e47d851ea79a5a0f5c
David Klein <david.klein@tu-braunschweig.de>
2020-10-13 16:50:57 +0200

    Added context information to JSON.stringify

--- js/src/builtin/JSON.cpp
+++ js/src/builtin/JSON.cpp
@@ -1146,7 +1146,7 @@ bool json_stringify(JSContext* cx, unsigned argc, Value* vp) {
     }
 
     // TaintFox: Add stringify operation to taint flows.
-    str->taint().extend(TaintOperation("JSON.stringify", true));
+    str->taint().extend(TaintOperationFromContext(cx, "JSON.stringify", true));
 
     args.rval().setString(str);
   } else {
