2ddd64b9521fa4ce26efb73ac130ebd662335c3b
soumboussaha <112937185+soumboussaha@users.noreply.github.com>
2023-01-13 12:16:19 +0100

    Update test.cpp

--- taint/test.cpp
+++ taint/test.cpp
@@ -12,21 +12,22 @@ using namespace std;
 // [{begin: 10, end: 20, source: 'src1'}, {begin: 80, end: 90, source: 'src2'}]
 
 int main() {
-/*
-string s =" [{begin: 10, end: 20, source: 'src1'}, {begin: 80, end: 90, source: 'src2'}]";
 
- StringTaint st = ParseTaint(s);
- */
+  string s ="[{begin: 10, end: 20, source: 'src1'}, {begin: 80, end: 90, source: 'src2'}]";
+
+  std::cout << "parsins this string to an object taint and back to a string " << s << std::endl;
+
+  StringTaint st = ParseTaint(s);
+
+  string str=serializeStringtaint(st);
+
+  std::cout << "DESERIALIZATION TESTED : stringTaintObject parsed correctly to a string object" << str << std::endl;
+
+  std::cout << "SERIALIZATION TESTED : String parsed back to a string succesfully " << serializeStringtaint(ParseTaint(str)) << std::endl;
 
 
-  TaintRange r=TaintRange(02,10,TaintOperation("src1"));
-  StringTaint s=StringTaint(r);
-  s.append(TaintRange(50,60,TaintOperation("src2")));
 
 
-  string str=serializeStringtaint(s);
-
-  std::cout << str << std::endl;
   
 
 }
