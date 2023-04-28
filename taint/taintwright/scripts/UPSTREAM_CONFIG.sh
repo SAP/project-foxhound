98da7f31d66eef5b1dab3dc9b5cf40a67a54a021
moorts <moritz.gutfleisch@gmx.de>
2023-04-28 09:44:19 +0000

    Clean up

--- taint/taintwright/scripts/UPSTREAM_CONFIG.sh
+++ taint/taintwright/scripts/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
-REMOTE_URL="https://github.com/moorts/project-foxhound"
+REMOTE_URL="https://github.com/SAP/project-foxhound"
 BASE_BRANCH="main"
 BASE_REVISION="37f943b5a377a56a207e0083da3f5a15379dcf48"
