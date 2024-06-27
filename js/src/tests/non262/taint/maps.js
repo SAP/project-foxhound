a442a54781420f24fffb37f12babafee962052c6
David Klein <david.klein@tu-braunschweig.de>
2024-06-27 13:17:40 +0200

    Added tests for failing playwright issue

--- js/src/tests/non262/taint/maps.js
+++ js/src/tests/non262/taint/maps.js
@@ -1,5 +1,4 @@
-function mapTaintTest() {
-
+function mapTaintedKeyTest() {
   let map = new Map(); 
   let tainted = taint(42);
   let untainted = 42;
@@ -10,7 +9,19 @@ function mapTaintTest() {
   assertEq(ret_val_tainted, ret_val_untainted);
 }
 
+function mapUntaintedKeyTest() {
+  let map = new Map(); 
+  let tainted = taint(42);
+  let untainted = 42;
+  let value = "foo";
+  map.set(untainted, value);
+  let ret_val_untainted = map.get(untainted)
+  let ret_val_tainted = map.get(tainted)
+  assertEq(ret_val_tainted, ret_val_untainted);
+}
+
 runTaintTest(mapTaintTest);
+runTaintTest(mapUntaintedKeyTest);
 
 
 if (typeof reportCompare === 'function')
