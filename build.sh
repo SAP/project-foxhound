b9b926d07bd862c2726c779e89e154348e3ddb11
David Klein <david.klein@tu-braunschweig.de>
2024-09-09 14:51:56 +0200

    Add build script for project foxhound.
    Currently one of the difficulties for people to get started with project
    foxhound seems to be the build. As how to build foxhound with playwright
    is something that's currently undocumented outside of the SAP CI and my
    adaption of it, this seems like a big hurdle to overcome.
    
    This PR aims to be the first step to automate most of the build
    requirements. It borrows heavily from Foxhound_builder, but as this
    script is supposed to live in tree we can simplify stuff greatly.
    
    We can, for example, make the playwright version available in machine
    readable format. This avoids the issue that each user has to update the build
    script on each release, because we can maintain the compatible
    playwright version. This can be extended to cover checks for usable
    rustc versions in the future.
    
    Generally, there are some additions that would be nice to have in the
    future:
    
    - Support to make a commit for the version we are trying to build.
    This matters in combination with playwright, as we avoid having an unclean
    tree. It also brings some issues, as we would have to detect that we are
    building on top of a branch used to build playwright to avoid throwing
    the branch away for each build (?)
    
    - Support to disable the preparation phase.
    If we want to test small changes iteratively, re-applying the
    playwright patches, running bootstrap etc is superfluous and can be
    avoided.
    
    - Additional safeguards.
    Such as tests for the correct rust version. This might be cumbersome as
    we would have to ensure the script picks up the same rust version mach
    does. It seems to favor the rustup based version over the version that
    is installed on the main OS. This requires testing, as this is only
    based on my observations so far.
    
    - Post build checks.
    
    For example, if we compile with playwright support, we can test
    `foxhound -juggler-pipe` and if the flag is recognized, the playwright
    integration is available.

---
+++ build.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+
+CURRENT_DIR=$(dirname "$(readlink -f "$0" || exit 1)")
+BASEDIR=$(realpath "${CURRENT_DIR}/..")
+
+RED=$(tput setaf 1)
+POWDER_BLUE=$(tput setaf 153)
+NORMAL=$(tput sgr0)
+
+_die() {
+    [[ -n $1 ]] || {
+        printf >&2 -- 'Usage:\n\t_die <message> [return code]\n'
+        [[ $- == *i* ]] && return 1 || exit 1
+    }
+
+    printf >&2 -- '%s\n' "${RED}$1${NORMAL}"
+    exit "${2:-1}"
+}
+
+_status() {
+  echo -e "${POWDER_BLUE}${1}${NORMAL}"
+}
+
+# Shared variables
+FOXHOUND_OBJ_DIR=
+FOXHOUND_DIR=
+PLAYWRIGHT_DIR=
+PLAYWRIGHT_VERSION=
+WITH_PLAYWRIGHT_INTEGRATION=
+NO_CLOBBER=
+
+_determine_obj_dir() {
+  if [ ! -d "${FOXHOUND_OBJ_DIR}" ]; then
+      OBJ_DIR="$(grep -v -e "^#" "${FOXHOUND_DIR}/.mozconfig" | grep "MOZ_OBJDIR=@TOPSRCDIR@" | cut -d "/" -f 2)"
+      if [ -z "${OBJ_DIR}" ]; then
+        _die "Unable to determine object directory from mozconfig, should be called MOZ_OBJDIR"
+      fi
+      #_status "FOXHOUND_OBJ_DIR not set, suggesting to set it to: '${OBJ_DIR}'" 1
+    fi
+    FOXHOUND_OBJ_DIR="$OBJ_DIR"
+}
+
+_make_git_commit() {
+  pushd "${FOXHOUND_DIR}" > /dev/null || _die "Can't change into foxhound dir: ${FOXHOUND_DIR}"
+  NEW_BRANCHNAME="pw-build-$(date +%s)" 
+  git switch -c "${NEW_BRANCHNAME}" || _die "Can't create new Git branch called: ${NEW_BRANCHNAME}"
+  git add .
+  git commit --author="Foxhound builder <foxhound@whereismymind.info>" --message="Added Playwright support to Foxhound."
+  popd > /dev/null || exit 1
+}
+
+_prepare_playwright() {
+  pushd "${FOXHOUND_DIR}" > /dev/null || _die "Can't change into foxhound dir: ${FOXHOUND_DIR}"
+  if [ ! -f "${CURRENT_DIR}/.PLAYWRIGHT_VERSION" ]; then
+    _die "Can not determine playwright version, as \".PLAYWRIGHT_VERSION\" file is missing. Please open an issue on Github!"
+  fi
+  . "${CURRENT_DIR}/.PLAYWRIGHT_VERSION"
+  _status "Set playwright version to: ${PLAYWRIGHT_VERSION}"
+  popd > /dev/null || exit 1
+  pushd "${PLAYWRIGHT_DIR}" > /dev/null || _die "Can't change into playwright dir: ${PLAYWRIGHT_DIR}"
+  git fetch --all || _die "Git fetch failed for Playwright"
+  git checkout "${PLAYWRIGHT_VERSION}" || _die "Can't checkout playwright version ${PLAYWRIGHT_VERSION}.."
+  popd > /dev/null || exit 1
+}
+
+_checkout_playwright() {
+  _status "Cloning playwright into ${PLAYWRIGHT_DIR}"
+  git clone https://github.com/microsoft/playwright.git "${PLAYWRIGHT_DIR}" || _die "Cloning playwright failed!"
+}
+
+_check_repo_foxhound_repo_state() {
+  pushd "${FOXHOUND_DIR}" > /dev/null || _die "Can't change into foxhound dir: ${FOXHOUND_DIR}"
+ test -z "$(git status --porcelain)" || _die "Dirty Worktree, please commit all changes before running the build script!"
+  popd > /dev/null || exit 1
+}
+
+_prepare_foxhound() {
+  pushd "${FOXHOUND_DIR}" > /dev/null || _die "Can't change into foxhound dir: ${FOXHOUND_DIR}"
+ _status "Preparing the foxhound build environment"
+  if [ -d "./juggler" ]; then
+    _status "Deleting stale juggler"
+    rm -rf "${FOXHOUND_DIR}/juggler"
+  fi
+  if [ ! -f "${FOXHOUND_DIR}/.mozconfig" ]; then
+    _status "Setting default mozconfig from Ubuntu profile"
+    cp "${FOXHOUND_DIR}/taintfox_mozconfig_ubuntu" "${FOXHOUND_DIR}/.mozconfig" 
+  fi
+  if [ -n "$NO_CLOBBER" ]; then
+    ./mach --no-interactive clobber
+  fi
+ ./mach --no-interactive bootstrap --no-system-changes --application-choice=browser
+ _determine_obj_dir
+ _status "Determined MOZ_OBJDIR as: $FOXHOUND_OBJ_DIR"
+  popd > /dev/null || exit 1
+}
+
+_patch_foxhound() {
+  _status "Patching foxhound for playwright integration"
+  pushd "${FOXHOUND_DIR}" > /dev/null || _die "Can't change into foxhound dir: ${FOXHOUND_DIR}"
+  cp -r "${PLAYWRIGHT_DIR}/browser_patches/firefox/juggler" "juggler"
+  git apply --verbose --index --whitespace=nowarn --recount "${PLAYWRIGHT_DIR}/browser_patches/firefox/patches"/* || _die "Playwright patches failed to apply."
+  popd > /dev/null || exit 1
+}
+
+_build_foxhound() {
+  pushd "${FOXHOUND_DIR}" > /dev/null || _die "Can't change into foxhound dir: ${FOXHOUND_DIR}"
+  ./mach build || _die "./mach build error"
+  popd > /dev/null || exit 1
+}
+
+_package_foxhound() {
+  pushd "${FOXHOUND_DIR}" > /dev/null || _die "Can't change into foxhound dir: ${FOXHOUND_DIR}"
+ _status "Packaging foxhound.."
+  ./mach package || _die "./mach package error"
+
+  if [ -n "$WITH_PLAYWRIGHT_INTEGRATION" ]; then
+    mkdir -p "${FOXHOUND_OBJ_DIR}/dist/foxhound/defaults/pref"
+    cp "${PLAYWRIGHT_DIR}/browser_patches/firefox/preferences/playwright.cfg" "${FOXHOUND_OBJ_DIR}/dist/foxhound/"
+    cp "${PLAYWRIGHT_DIR}/browser_patches/firefox/preferences/00-playwright-prefs.js" "${FOXHOUND_OBJ_DIR}/dist/foxhound/defaults/pref/"
+  fi
+  pushd "${FOXHOUND_OBJ_DIR}/dist" || exit 1
+  zip -r foxhound_linux.zip foxhound
+  _status "Zip located at '$(pwd  || true)/foxhound_linux.zip', done!"
+  popd > /dev/null || exit 1
+  popd > /dev/null || exit 1
+}
+
+
+
+main() {
+  FOXHOUND_DIR="${CURRENT_DIR}"
+  PLAYWRIGHT_DIR="${BASEDIR}/playwright"
+  if [ -n "$WITH_PLAYWRIGHT_INTEGRATION" ] && [ ! -d "${PLAYWRIGHT_DIR}" ]; then
+    _checkout_playwright
+  fi
+  _prepare_foxhound
+  if [ -n "$WITH_PLAYWRIGHT_INTEGRATION" ]; then
+    _prepare_playwright
+    _patch_foxhound
+  fi
+  _build_foxhound
+  _package_foxhound
+}
+
+_help() {
+   echo "Builds project foxhound"
+   echo
+   echo "Syntax: build.sh [-h|c|p]"
+   echo "options:"
+   echo "c     Does not clobber the build prior to building."
+   echo "h     Print this Help."
+   echo "p     Builds with playwright integration."
+   echo
+}
+
+while getopts "hpc" option; do
+  case $option in
+    h)
+        _help
+        exit;;
+    c) 
+        NO_CLOBBER=1;;
+    p) 
+        WITH_PLAYWRIGHT_INTEGRATION=1;;
+    \?) 
+        echo "Error: Invalid option"
+        exit;;
+  esac
+done
+main
+
