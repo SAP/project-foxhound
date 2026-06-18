53d15868bc5eb487bfdad291352a8ff87855d6b5
Segun Famisa <sfamisa@mozilla.com>
2026-06-18 19:25:34 +0000

    Bug 2039867 - Simplify ImporterMiddleware delay strategy and drop ImportCancelled action when no import is active  a=RyanVM
    There are two fixes to address the two scenarios where bug 2039867 are happening:
    
    1. Do not start the minimum wait delay together with the importation. The minimum wait/delay we add breaks when the import completes early and the user cancels just after. The operation is already finished and there is nothing to cancel.
    
    2. We need to ensure that we don't update the state if we did not really cancel anything i.e if the import job was not in progress.
    
    Original Revision: https://phabricator.services.mozilla.com/D305178
    
    Differential Revision: https://phabricator.services.mozilla.com/D306154

--- mobile/android/android-components/components/feature/importer/src/main/java/mozilla/components/feature/importer/ImporterMiddleware.kt
+++ mobile/android/android-components/components/feature/importer/src/main/java/mozilla/components/feature/importer/ImporterMiddleware.kt
@@ -6,8 +6,6 @@ package mozilla.components.feature.importer
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import mozilla.components.concept.bookmarks.file.BookmarksFileImporter
@@ -31,8 +29,7 @@ class ImporterMiddleware(
         next: (ImporterAction) -> Unit,
         action: ImporterAction,
     ) {
-        next(action)
-        when (action) {
+        val actionResult = when (action) {
             is ImporterAction.FileSelected -> {
                 importJob = lifecycleScope.launch {
                     store.dispatch(ImporterAction.ImportStarted)
@@ -40,20 +37,37 @@ class ImporterMiddleware(
                     // We want to make sure we stay in the loading state for at least one second
                     // during an import to prevent the dialog from flashing before the user can
                     // comprehend what is currently happening.
-                    val minimumWait = async { delay(1.seconds) }
-                    val result = async { importer.importBookmarksFromUri(action.uri) }
-
-                    awaitAll(minimumWait, result)
-
-                    result.await()
-                        .onFailure { store.dispatch(ImporterAction.ImportFailed) }
-                        .onSuccess { store.dispatch(ImporterAction.ImportFinished(it.count)) }
+                    // Previous implementation of this waiting strategy in bug 2035342 used
+                    // the async delay awaitAll approach.
+                    // However, we often ran into edge cases where users canceled within the
+                    // "minimum wait" time, and because we already started (maybe finished) inserting
+                    // the bookmarks, the cancelation had no effect, thereby, confusing the users
+                    // We will try and simplify using a simple delay to achieve a similar effect.
+                    delay(1.seconds)
+                    importer.importBookmarksFromUri(action.uri)
+                        .onFailure {
+                            store.dispatch(ImporterAction.ImportFailed)
+                        }
+                        .onSuccess {
+                            store.dispatch(ImporterAction.ImportFinished(it.count))
+                        }
                 }
+                ActionResult.ContinueChain
             }
 
             is ImporterAction.ImportCancelled -> {
-                importJob?.cancel()
-                importJob = null
+                // Bug 2039867: we want to make sure that if the import is ongoing, we cancel the job
+                // and propagate the canceled state.
+                //
+                // Otherwise, we swallow the action and do not pass it down
+                // to prevent state change, since we did not really cancel anything.
+                if (importJob?.isActive == true) {
+                    importJob?.cancel()
+                    importJob = null
+                    ActionResult.ContinueChain
+                } else {
+                    ActionResult.SwallowAction
+                }
             }
 
             ImporterAction.FileSelectionCanceled,
@@ -61,7 +75,30 @@ class ImporterMiddleware(
             ImporterAction.ViewAppeared,
             is ImporterAction.ImportFinished,
             ImporterAction.ImportFailed,
-                -> Unit
+                -> ActionResult.ContinueChain
         }
+
+        when (actionResult) {
+            ActionResult.ContinueChain -> next(action)
+            else -> Unit
+        }
+    }
+
+    /**
+     * Enum type to decide what we do after processing an action. We need this in this middleware
+     * because there are cases where we want to prevent an action from being passed down the
+     * chain (reducer + middleware).
+     */
+    private enum class ActionResult {
+
+        /**
+         * A result based on which, we want to continue the reducer + middleware chain
+         */
+        ContinueChain,
+
+        /**
+         * A result based on which, we want to drop the action from the chain and swallow it
+         */
+        SwallowAction,
     }
 }
