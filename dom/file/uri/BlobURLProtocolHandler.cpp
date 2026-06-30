465d75832db879531d45949ef741c3ed8fd579ee
Nika Layzell <nika@thelayzells.com>
2026-06-30 12:55:37 +0000

    Bug 2006467 - Part 1: Use an arbitrary null principal for blob:system URIs,  a=pascalc
    This patch changes the URL which resources loaded from `blob:system` URIs are
    given, and does not change the internal URI stored within the data store. This
    keeps the existing "broadcast principal" behaviour, while restricting the
    principal given to data loaded from a `blob:system` URI.
    
    Unlike blob:null, a blob:system URL does not have a stable null principal.
    Instead, it has a unique null principal every time it is loaded. This behaviour
    is intended to be more similar to the behaviour of e.g. data: URIs.
    
    I am optimistic that this will be OK given that the blob URL is only intended
    to be used by system code, so shouldn't be exercised in ways which depends on
    multiple blob URLs created by the same origin being able to access one-another.
    
    Original Revision: https://phabricator.services.mozilla.com/D300875
    
    Differential Revision: https://phabricator.services.mozilla.com/D309050

--- dom/file/uri/BlobURLProtocolHandler.cpp
+++ dom/file/uri/BlobURLProtocolHandler.cpp
@@ -898,7 +898,10 @@ bool BlobURLProtocolHandler::GetBlobURLPrincipal(nsIURI* aURI,
 
   nsDependentCSubstring originPart = blobURL->OriginPart();
   if (originPart == "system"_ns) {
-    principal = nsContentUtils::GetSystemPrincipal();
+    // This BlobURL was created with a system principal, and can only actually
+    // be loaded by the system principal, but exposes a resource with an
+    // arbitrary null principal.
+    principal = NullPrincipal::Create(aAttrs);
   } else if (originPart == "null"_ns) {
     // If the origin part is the string "null", the principal should have been
     // cached on the URI when it was parsed.
