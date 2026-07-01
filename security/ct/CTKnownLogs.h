ed56857fa1dddcb8a64cb131c71de3ea15dadf0c
ffxbld <ffxbld@mozilla.com>
2026-07-01 02:16:44 +0000

    No Bug, mozilla-beta repo-update remote-settings mobile-merino-manifest ct-logs -  a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D309686

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1788725070000000);
+static const PRTime kCTExpirationTime = INT64_C(1788897956000000);
 
 namespace mozilla::ct {
 
