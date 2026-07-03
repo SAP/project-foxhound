623c35cba2c083bae13416fc6165ea3a9318101b
ffxbld <ffxbld@mozilla.com>
2026-07-03 02:23:54 +0000

    No Bug, mozilla-beta repo-update remote-settings tld-suffixes mobile-experiments ct-logs -  a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D310159

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1788897956000000);
+static const PRTime kCTExpirationTime = INT64_C(1789070753000000);
 
 namespace mozilla::ct {
 
