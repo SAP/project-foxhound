00c784e13fb9b3716434354b75805ecb1ac92f4e
Ryan Curran <rcurran@mozilla.com>
2026-07-06 18:04:22 +0000

    Bug 2048072 - Skip test_switch_tabs_for_new_background_window_without_focus_change on macOS VM workers  a=pascalc
    Beta has none of the macOS-VM skip infrastructure that landed on central this
    cycle, so add skip_if_macos_vm (detecting the VM via sysctl hw.model =
    VirtualMac) and apply it to the racy test directly. This matches the final
    central state from D309561.
    
    Differential Revision: https://phabricator.services.mozilla.com/D309888

--- testing/marionette/harness/marionette_harness/marionette_test/__init__.py
+++ testing/marionette/harness/marionette_harness/marionette_test/__init__.py
@@ -11,6 +11,7 @@ from .decorators import (
     run_if_manage_instance,
     skip_if_chrome,
     skip_if_desktop,
+    skip_if_macos_vm,
     skip_unless_browser_pref,
     skip_unless_protocol,
     with_parameters,
