9783926affd3998e1c368570ae8f02f45610c8b5
Matthew Gaudet <mgaudet@mozilla.com>
2026-07-09 14:40:29 +0000

    Bug 2052375 - Partially Revert "Bug 1996693 - JS::PersistentRooted from PullWithDefaultReaderChunkStepMicrotask::mChunk"  a=pascalc
    Keep the test though.
    
    Original Revision: https://phabricator.services.mozilla.com/D310701
    
    Differential Revision: https://phabricator.services.mozilla.com/D311286

--- dom/streams/ReadableStreamTee.cpp
+++ dom/streams/ReadableStreamTee.cpp
@@ -73,13 +73,13 @@ void ReadableStreamDefaultTeeReadRequest::ChunkSteps(
       : public MicroTaskRunnable {
     // Virtually const, but is cycle collected
     MOZ_KNOWN_LIVE RefPtr<TeeState> mTeeState;
-    JS::Heap<JS::Value> mChunk;
+    JS::PersistentRooted<JS::Value> mChunk;
 
    public:
     ReadableStreamDefaultTeeReadRequestChunkSteps(JSContext* aCx,
                                                   TeeState* aTeeState,
                                                   JS::Handle<JS::Value> aChunk)
-        : mTeeState(aTeeState), mChunk(aChunk) {}
+        : mTeeState(aTeeState), mChunk(aCx, aChunk) {}
 
     MOZ_CAN_RUN_SCRIPT
     void Run(AutoSlowOperation& aAso) override {
@@ -136,10 +136,6 @@ void ReadableStreamDefaultTeeReadRequest::ChunkSteps(
     bool Suppressed() override {
       nsIGlobalObject* global = mTeeState->GetStream()->GetParentObject();
       return global && global->IsInSyncOperation();
-    }
-
-    void TraceMicroTask(JSTracer* trc) override {
-      TraceEdge(trc, &mChunk, "ReadableStreamDefaultTeeReadRequest Chunk");
     }
   };
 
