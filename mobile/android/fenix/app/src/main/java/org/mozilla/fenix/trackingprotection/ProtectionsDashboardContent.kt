e5eb348ac0a5fb6a5cb042e927b319fb2a0ebd2b
Mugurell <Mugurell@users.noreply.github.com>
2026-07-09 14:43:25 +0000

    Bug 2050250 - Ensure the bottom sheet handle of the protections dashboard has 48dp height  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D309785
    
    Differential Revision: https://phabricator.services.mozilla.com/D311244

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/trackingprotection/ProtectionsDashboardContent.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/trackingprotection/ProtectionsDashboardContent.kt
@@ -8,10 +8,10 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -96,7 +96,7 @@ fun ProtectionsDashboardContent(
                 contentDescription = "",
                 modifier = Modifier
                     .align(Alignment.TopCenter)
-                    .padding(top = FirefoxTheme.layout.size.static200)
+                    .minimumInteractiveComponentSize()
                     .semantics { traversalIndex = 1f },
             )
         }
