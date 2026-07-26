29658300527c466181a68de43c36181f5b3007e5
scottdowne <sdowne@mozilla.com>
2026-07-26 16:46:57 +0000

    Bug 2056650 - Mark moz-newtab-wallpaper as known-cacheable so newtab custom wallpapers stop flashing  a=pascalc DONTBUILD
    Newtab has its own special wallpaper protocol for custom wallpapers, and it
    was missing from the knownCacheable detection. Combined with newtab
    preloading, that made the image periodically fail ValidateEntry and get
    re-requested, causing the flash.
    
    Original Revision: https://phabricator.services.mozilla.com/D313305
    
    Differential Revision: https://phabricator.services.mozilla.com/D314081

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -12866,6 +12866,7 @@ nsContentUtils::GetSubresourceCacheValidationInfo(nsIRequest* aRequest,
       return false;
     }
     if (aURI->SchemeIs("data") || aURI->SchemeIs("moz-page-thumb") ||
+        aURI->SchemeIs("moz-newtab-wallpaper") ||
         aURI->SchemeIs("moz-extension")) {
       return true;
     }
